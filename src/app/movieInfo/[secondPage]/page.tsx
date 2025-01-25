9c7755268c3371cb9617ad5921e2e80aed9626e3
Sapar Smagul <smagulsapar52@gmail.com>
2025-01-25 16:54:40 +0800

    error remove

--- src/app/movieInfo/[secondPage]/page.tsx
+++ src/app/movieInfo/[secondPage]/page.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 const SecondPage = async ({
   params: { secondPage },
 }: {
-  params: { secondPage: string | number };
+  params: { secondPage: number };
 }) => {
   const movieDetail = `/movie/${secondPage}?language=en-US`;
   const movie = await fetchData(movieDetail);
